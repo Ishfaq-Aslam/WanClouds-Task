c7a7a2573acb3a7aeba937738f709986ff7c2408
Ishfaq-Aslam <ishfaqaslam0786@gmail.com>
2022-04-20 22:58:45 +0500

    firstcommit

---
+++ views.py
@@ -0,0 +1,10 @@
+# from app.models import USER
+# from app import app
+# from app import models
+
+# CREATE TABLE user (
+# id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+# firstname VARCHAR(30) NOT NULL,
+# lastname VARCHAR(30) NOT NULL,
+# email VARCHAR(50),
+# reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
